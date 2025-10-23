8ec986ed83ad9a67307667b3970d5012e23c2da0
Alcino-Inquilino <jp1_dantas@hotmail.com>
2025-10-23 16:42:16 -0300

    Teste do git add sem o .

--- cod_ana_maria.py
+++ cod_ana_maria.py
@@ -1,14 +1,14 @@
 from TADdeque import Deque
 
-def maximo_d_deslizante(nums: list[int], k: int) -> list[int]:
+def max_janela(nums: list[int], k: int) -> list[int]:
     '''
     Retorna o maior elemento da d deslizante de tamanho *k*
     que percorre a lista *nums* da esquerda para à direita
-    >>> maximo_d_deslizante([23, 32, 11, 11, 34, 43], 1)
+    >>> max_janela([23, 32, 11, 11, 34, 43], 1)
     [23, 32, 11, 11, 34, 43]
-    >>> maximo_d_deslizante([23, 32, 11, 11, 34, 43], 2)
+    >>> max_janela([23, 32, 11, 11, 34, 43], 2)
     [32, 32, 11, 34, 43]
-    >>> maximo_d_deslizante([2, 1, -1, 3, 4, 6, -2, 5], 3)
+    >>> max_janela([2, 1, -1, 3, 4, 6, -2, 5], 3)
     [2, 3, 4, 6, 6, 6]
     '''
     d = Deque()
@@ -17,9 +17,10 @@ def maximo_d_deslizante(nums: list[int], k: int) -> list[int]:
         return []
     
     for i in range(len(nums)):
-        if not d.vazia() and d.consulta_inicio() <= i - k: #Verifica se o elemento máximo faz parte da próxima d
-            d.remove_inicio()
-        while not d.vazia() and nums[i] > nums[d.consulta_fim()]: #Verifica se o elemento da lista é maior que o máximo atual armazenado no deque
+        if not d.vazia() and d.consulta_inicio() <= i - k: #Verifica se o elemento máximo faz parte da janela
+            d.remove_inicio()                              #Não realiza a iteração com a lista vazia para não lançar um erro
+        
+        while not d.vazia() and nums[i] > nums[d.consulta_fim()]: #Verifica se o elemento da lista é candidato a ser o maior     
             d.remove_fim()
         d.insere_fim(i)
         if i >= k - 1: #Começa a retornar apenas após o tamanho(k) especificado da d, o -1 é necessário pois o índice da lista começa em 0
